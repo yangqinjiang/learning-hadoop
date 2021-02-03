fc05c123ffcb6a21f68f472a51d6f6e19e415b58
yangqinjiang <531553280@qq.com>
2021-02-03 13:45:02 +0800

    自定义outputFormat

---
+++ src/main/java/mapreduce/outputformat/FilterMapper.java
@@ -0,0 +1,18 @@
+package mapreduce.outputformat;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class FilterMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+
+	@Override
+	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, NullWritable>.Context context)
+			throws IOException, InterruptedException {
+		// 写出
+		context.write(value, NullWritable.get());
+	}
+}
