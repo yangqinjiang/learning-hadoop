e07d255bbadc7ed766e3dd254718c42f14f20fac
yangqinjiang <531553280@qq.com>
2021-02-03 15:54:49 +0800

    reduce join

--- src/main/java/mapreduce/outputformat/FilterDriver.java
+++ src/main/java/mapreduce/outputformat/FilterDriver.java
@@ -1,7 +1,5 @@
 package mapreduce.outputformat;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
